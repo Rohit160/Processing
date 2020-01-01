53dafbd92e61d99fff9a194650fabbaaf45ae709
ARSH SHARMA <184517@nith.ac.in>
2020-01-01 15:18:11 +0530

    final debug

--- raytracing maze/particle.js
+++ raytracing maze/particle.js
@@ -1,47 +1,77 @@
 class Particle {
     constructor() {
-        this.pos = createVector(width/2, height/2);
-        this.rays = [];
-        for(let a = 0; a < 360 ; a += 1) {
-            this.rays.push(new Ray(this.pos, radians(a)));
-
-        }
+      this.fov = 45;
+      this.pos = createVector(sceneW / 2, sceneH / 2);
+      this.rays = [];
+      this.heading = 0;
+      for (let a = -this.fov / 2; a < this.fov / 2; a += 1) {
+        this.rays.push(new Ray(this.pos, radians(a)));
+      }
     }
-
-    update(x, y)
-    {
-        this.pos.set(x, y)
+  
+    updateFOV(fov) {
+      this.fov = fov;
+      this.rays = [];
+      for (let a = -this.fov / 2; a < this.fov / 2; a += 1) {
+        this.rays.push(new Ray(this.pos, radians(a) + this.heading));
+      }
     }
-
-    look(walls){
-         for(let ray of this.rays){
-            let record = Infinity; 
-            let closest = null;
-            for(let wall of walls){
-                
-                const point = ray.cast(wall)
-                if(point){
-                    const d = p5.Vector.dist(this.pos, point);
-                    if(d<record){
-                        record = d;
-                        closest = point;
-                    }
-                    //record = min(d, record);
-                    }
-        
-                }
-                if(closest){
-                    stroke(255, 100)
-                line(this.pos.x, this.pos.y, closest.x, closest.y);
-                }
-
-    }}
-
-    show() {
-        fill(255);
-        ellipse(this.pos.x, this.pos.y, 4);
-            for(let ray of this.rays){
-                ray.show();
+  
+    rotate(angle) {
+      this.heading += angle;
+      let index = 0;
+      for (let a = -this.fov / 2; a < this.fov / 2; a += 1) {
+        this.rays[index].setAngle(radians(a) + this.heading);
+        index++;
+      }
+    }
+  
+    move(amt) {
+      const vel = p5.Vector.fromAngle(this.heading);
+      vel.setMag(amt);
+      this.pos.add(vel);
+    }
+  
+    update(x, y) {
+      this.pos.set(x, y);
+    }
+  
+    look(walls) {
+      const scene = [];
+      for (let i = 0; i < this.rays.length; i++) {
+        const ray = this.rays[i];
+        let closest = null;
+        let record = Infinity;
+        for (let wall of walls) {
+          const pt = ray.cast(wall);
+          if (pt) {
+            let d = p5.Vector.dist(this.pos, pt);
+            const a = ray.dir.heading() - this.heading;
+            if (!mouseIsPressed) {
+              d *= cos(a);
             }
+            if (d < record) {
+              record = d;
+              closest = pt;
+            }
+          }
         }
+        if (closest) {
+          // colorMode(HSB);
+          // stroke((i + frameCount * 2) % 360, 255, 255, 50);
+          stroke(255, 100);
+          line(this.pos.x, this.pos.y, closest.x, closest.y);
+        }
+        scene[i] = record;
+      }
+      return scene;
     }
+  
+    show() {
+      fill(255);
+      ellipse(this.pos.x, this.pos.y, 4);
+      for (let ray of this.rays) {
+        ray.show();
+      }
+    }
+  }
--- raytracing maze/sketch.js
+++ raytracing maze/sketch.js
@@ -1,43 +1,75 @@
+let walls = [];
 let ray;
-let walls= [];  //boundary_obs
 let particle;
-let xoff = 5000;
-let yoff = 100000;
-function setup()
-{
-  createCanvas(1366, 768);
-  for(let i =0; i<5; i++){
-    let x1 = random(width);
-    let x2 = random(width);
-    let y1 = random(height);
-    let y2 = random(height);
-    walls[i] = new Boundary(x1, y1, x2, y2)
+let xoff = 0;
+let yoff = 10000;
+
+const sceneW = 400;
+const sceneH = 400;
+let sliderFOV;
+
+function setup() {
+  createCanvas(800, 400);
+  for (let i = 0; i < 5; i++) {
+    let x1 = random(sceneW);
+    let x2 = random(sceneW);
+    let y1 = random(sceneH);
+    let y2 = random(sceneH);
+    walls[i] = new Boundary(x1, y1, x2, y2);
   }
-  walls.push(new Boundary(0, 0 , width, 0));
-  walls.push(new Boundary(width, 0 , width, height));
-  walls.push(new Boundary(width, height , 0, height));
-  walls.push(new Boundary(0, height , 0, 0)); 
-  // //ray = new Ray(100, 200)
-  // wall = new Boundary(300,100,300,300);
-   particle = new Particle();
+  walls.push(new Boundary(0, 0, sceneW, 0));
+  walls.push(new Boundary(sceneW, 0, sceneW, sceneH));
+  walls.push(new Boundary(sceneW, sceneH, 0, sceneH));
+  walls.push(new Boundary(0, sceneH, 0, 0));
+  particle = new Particle();
+  sliderFOV = createSlider(0, 360, 45);
+  sliderFOV.input(changeFOV);
 }
-function draw()
-{
-  
-  background(0);
-  for(let wall of walls ){
-  wall.show();
+
+function changeFOV() {
+  const fov = sliderFOV.value();
+  particle.updateFOV(fov);
+}
+
+function draw() {
+  if (keyIsDown(LEFT_ARROW)) {
+    particle.rotate(-0.1);
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    particle.rotate(0.1);
+  } else if (keyIsDown(UP_ARROW)) {
+    particle.move(2);
+  } else if (keyIsDown(DOWN_ARROW)) {
+    particle.move(-2);
   }
-  particle.update(noise(xoff)*width, noise(yoff)*height);
+
+  background(0);
+  for (let wall of walls) {
+    wall.show();
+  }
   particle.show();
-  particle.look(walls);
-  xoff +=.01;
-  yoff +=.01;
-  // //ray.lookAt(mouseX, mouseY);
-  // let point = ray.cast(wall);
-  // //console.log(point);
-  // if(point){
+
+  const scene = particle.look(walls);
+  const w = sceneW / scene.length;
+  push();
+  translate(sceneW, 0);
+  for (let i = 0; i < scene.length; i++) {
+    noStroke();
+    const sq = scene[i] * scene[i];
+    const wSq = sceneW * sceneW;
+    const b = map(sq, 0, wSq, 255, 0);
+    const h = map(scene[i], 0, sceneW, sceneH, 0);
+    fill(b);
+    rectMode(CENTER);
+    rect(i * w + w / 2, sceneH / 2, w + 1, h);
+  }
+  pop();
+
+  // ray.show();
+  // ray.lookAt(mouseX, mouseY);
+
+  // let pt = ray.cast(wall);
+  // if (pt) {
   //   fill(255);
-  //   ellipse(point.x, point.y, 8, 8);
+  //   ellipse(pt.x, pt.y, 8, 8);
   // }
 }
